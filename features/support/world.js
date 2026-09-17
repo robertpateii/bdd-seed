4787d11f7ec20b3602ef0fbc0d9e8c8b96aba721
Robert Pate <robert.pate@gmail.com>
2026-09-16 19:10:51 -0500

    Replace hamjest with assert; cut unused requires

--- features/support/world.js
+++ features/support/world.js
@@ -1,8 +1,9 @@
 const {setWorldConstructor, World } = require('@cucumber/cucumber');
-const {Builder, By, Until, Browser} = require('selenium-webdriver');
+const {Builder, Browser} = require('selenium-webdriver');
 const Chrome = require('selenium-webdriver/chrome');
 const coptions = new Chrome.Options();
 
+// Runs around every scenario, providing this as a reference to the scenario
 class CustomWorld extends World {
     // exclude enable-logging (turns off logging) to avoid non-test-related output from Chrome
     driver = new Builder()
@@ -16,12 +17,6 @@ class CustomWorld extends World {
         super(options)
     }
     
-    // Returns a promise that resolves to the element
-    async waitForElement(locator) {
-        const condition = Until.elementLocated(locator)
-        return await this.driver.wait(condition)
-    }
-
     async getPage(page) {
         return await this.driver.get(page);
     }
